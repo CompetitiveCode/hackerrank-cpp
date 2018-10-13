ce6c48cdf568d1fb932caa51fafae266713dba9d
Shebin John <admin@remedcu.com>
2018-10-14 00:21:41 +0530

    14th October 2018

--- Introduction/Arrays Introduction.cpp
+++ Introduction/Arrays Introduction.cpp
@@ -15,3 +15,17 @@ int main() {
     }
     return 0;
 }
+
+/*
+An array is a series of elements of the same type placed in contiguous memory locations that can be individually referenced by adding an index to a unique identifier.
+
+Declaration:
+
+int arr[10]; //Declares an array named arr of size 10, i.e; you can store 10 integers.
+
+Accessing elements of an array:
+
+Indexing in arrays starts from 0.So the first element is stored at arr[0],the second element at arr[1]...arr[9]
+
+You'll be given an array of N integers and you have to print the integers in the reverse order.
+*/
